211a5122a1dce2af0ad10ddb9d8133acdaa458a0
Zelalem Belay <zelalem.belay1@gmail.com>
2017-02-03 13:58:34 -0500

    Test class added

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Zelalem Belay on 2/3/2017.
+ */
+
+public class MainTest {
+
+    @Test
+    public void testPrintMessage() {
+        System.out.println("Inside testPrintMessage()");
+        assertEquals("aa","a");
+    }
+}
